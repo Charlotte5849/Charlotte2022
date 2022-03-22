6b607182715bd16d55e328c45ceb973f8723a73f
Charlotte5849 <75740063+Charlotte5849@users.noreply.github.com>
2022-03-22 12:14:31 +0800

    Add files via upload
    It's the material that is required when running Student.py

---
+++ TMU.py
@@ -0,0 +1,8 @@
+class TMU():
+    def __init__(self, name, number):
+        self.name = name
+        self.number = number
+
+    def show(self):
+        print("name:", self.name)
+        print("number:", self.number)
