8d4624c2b51af2daee6d48bb190ac015931be551
Charlotte5849 <75740063+Charlotte5849@users.noreply.github.com>
2022-03-22 12:17:00 +0800

    Create Call a Function.py

---
+++ Call a Function.py
@@ -0,0 +1,11 @@
+from TMU import TMU
+class student(TMU):
+  def __init__(self, name, number):
+        self.name = name
+        self.number = number
+
+  def show(self):
+        print("name:", self.name)
+        print("number:", self.number)
+charlotte=student("CharlotteChen","B812108030")
+charlotte.show()
