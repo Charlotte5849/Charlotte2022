567e28cb265b90ac4a545d20fc393822968dc2b9
PeiAn Chen <75740063+Charlotte5849@users.noreply.github.com>
2022-09-06 18:02:34 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  Prints "Hello, World". 
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *  Name:              Pei-An Chen
+ *  Last modified:     September 6, 2022
+ *  Purpose: Coursera Course Assignments
+ *  Course: Algorithms, Part I (Princeton University)
+ ******************************************************************************/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
