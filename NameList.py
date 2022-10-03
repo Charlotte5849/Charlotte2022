aa24cd6028374ca6143ec76a8f1360c564c614b5
PeiAn Chen <75740063+Charlotte5849@users.noreply.github.com>
2022-10-03 22:15:00 +0800

    Delete NameList.py

--- NameList.py
+++
@@ -1,8 +0,0 @@
-class NameList():
-    def __init__(self, name, number):
-        self.name = name
-        self.number = number
-
-    def show(self):
-        print("name:", self.name)
-        print("number:", self.number)
